4659f093124bd224a9d406c8170ddc9f967bf5cf
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-04 21:08:56 +0530

    Update MHSA.py

--- unet/vanilla_transformer/MHSA.py
+++ unet/vanilla_transformer/MHSA.py
@@ -2,3 +2,15 @@ import numpy as np
 import torch
 from einops import rearrange
 from torch import nn
+
+def compute_mhsa(q, k, v, scale_factor=1, mask=None):
+    # resulted shape will be: [batch, heads, tokens, tokens]
+    scaled_dot_prod = torch.einsum('... i d , ... j d -> ... i j', q, k) * scale_factor
+
+    if mask is not None:
+        assert mask.shape == scaled_dot_prod.shape[2:]
+        scaled_dot_prod = scaled_dot_prod.masked_fill(mask, -np.inf)
+
+    attention = torch.softmax(scaled_dot_prod, dim=-1)
+    # calc result per head
+    return torch.einsum('... i j , ... j d -> ... i d', attention, v)
